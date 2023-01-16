abffc77fd292f17c8e6ce6741d84e05fc6648d24
Ruken Erpolat <72344293+rukenerpolat@users.noreply.github.com>
2023-01-16 20:46:05 +0300

    "color changes - number is a multiple of 10."

--- app.js
+++ app.js
@@ -5,17 +5,30 @@ let btnIncrease = document.querySelector(".increase");
 let btnDecrease = document.querySelector(".decrease");
 let btnReset = document.querySelector(".reset");
 
+
 btnIncrease.addEventListener("click", () => {
     value++;
+
+    value % 10 === 0
+        ? (numberContainer.style.color = "#8c3c23")
+        : (numberContainer.style.color = "#406485");
+
     numberContainer.textContent = value;
 });
 
 btnDecrease.addEventListener("click", () => {
+
     value--;
+
     if (value < 0) {
         value = 0;
         alert("The number cannot be less than zero.");
     }
+
+    value % 10 === 0
+        ? (numberContainer.style.color = "#8c3c23")
+        : (numberContainer.style.color = "#406485");
+
     numberContainer.textContent = value;
 });
 
